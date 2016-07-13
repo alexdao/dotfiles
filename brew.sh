66bc9527abc8803a2799709d4247831304977f36
Alex Dao <azd2@duke.edu>
2016-07-12 22:59:38 -0700

    Add brew install script

---
+++ brew.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Download fish, add it as a shell, and set it as default
+brew install fish
+if ! fgrep -q '/usr/local/bin/fish' /etc/shells; then
+  echo '/usr/local/bin/fish' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/fish;
+fi;
+
+# Install Bash 4.
+brew install bash
+brew tap homebrew/versions
+
+# Install more recent versions of some macOS tools.
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php56 --with-gmp
+
+# Install other useful binaries.
+brew install ack
+brew install cheat
+brew install dark-mode
+brew install diff-so-fancy
+# Set diff-so-fancy colors
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+brew install git
+brew install git-lfs
+brew install mobile-shell
+brew install p7zip
+brew install speedtest_cli
+brew install testssl
+brew install tree
+brew install watchman
+brew install wget
+
+# Remove outdated versions from the cellar.
+brew cleanup
--- install-list.md
+++ install-list.md
@@ -1,20 +1,3 @@
 ### Themes
 * iTerm3/zsh: [Honukai](https://github.com/oskarkrawczyk/honukai-iterm-zsh)
 * Jetbrains: [Base16 Ocean](http://color-themes.com/?view=theme&id=563a1a7880b4acf11273ae9b)
-
-### Homebrew
-* cheat		
-* dark-mode
-* fish
-*	gdbm
-* mobile-shell (mosh)
-* openssl
-* readline
-* xz
-* coreutils
-* diff-so-fancy
-* moreutils
-*	watchman
-*	gdb		
-* macvim
-*	wget
